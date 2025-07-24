2dac2b599b0a635ba314dd4e24dafa894ee0aae2
D3zD3adWinn3r <Desireenlopez5@gmail.com>
2025-07-24 17:24:49 -0500

    added convert table

--- index.js
+++ index.js
@@ -2,4 +2,16 @@ function convertToCelsius(fahrenheit) {
     return (fahrenheit - 32) * 5 / 9;
 }
 
-function 
+function describeTemperature(fahrenheit) {
+    const celsius = convertToCelsius(fahrenheit);
+    if (celsius < 0) {
+        return "very cold!";
+    } else if (celsius < 20) {
+        return "cold";
+    } else if (celsius < 30) {
+        return "warm";
+    } else if (celsius < 40) {
+        return "hot";
+    } else if (celsius >= 40) {
+        return "very hot";
+    }
